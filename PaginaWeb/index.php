33846c06c410f1c6d8aacbcdf5ad0f1c52289eff
DiscoverExRich <torrehuelamo@yahoo.es>
2014-03-19 17:27:54 +0100

    Version0.1.1
    Cambios en estilos y parrafos de index.php y plantillaBase.php

--- PaginaWeb/index.php
+++ PaginaWeb/index.php
@@ -8,8 +8,9 @@
 <!-- InstanceBeginEditable name="head" -->
 <!-- InstanceEndEditable -->
 <link href="css/estiloPrincipal.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="css/menu.css" type="text/css" />
-<link href='http://fonts.googleapis.com/css?family=Bevan' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/menu.css" type="text/css" >
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css'>
 <?php include("includes/google.php"); ?>
 </head>
 
@@ -22,11 +23,17 @@
 	<?php include("includes/menu.php"); ?>
       
   </div>
-  <div class="sidebar1">
-        
-    <p> Los v&iacute;nculos anteriores muestran una estructura de navegaci&oacute;n b&aacute;sica que emplea una lista no ordenada con estilo de CSS. Util&iacute;cela como punto de partida y modifique las propiedades para lograr el aspecto deseado. Si necesita men&uacute;s desplegables, cr&eacute;elos empleando un men&uacute; de Spry, un widget de men&uacute; de Adobe Exchange u otras soluciones de javascript o CSS.</p>
-    <p>Si desea que la navegaci&oacute;n se sit&uacute;e a lo largo de la parte superior, simplemente mueva ul.nav a la parte superior de la p&aacute;gina y vuelva a crear el estilo.</p>
-    <!-- end .sidebar1 --></div>
+  <div class="sidebar1"><!-- InstanceBeginEditable name="ContenidoIzq" -->
+    <h1>Su Proyecto Web</h1>
+    <p>AyZWeb est&aacute; formado por un programador freelance experimentado en dise&ntilde;o y desarrollo&nbsp;<strong>Web</strong>. Gracias a mi formaci&oacute;n, experiencia y contactos, puedo ofrecer un servicio integral de desarrollo personalizado.</p>
+    <p>&iquest;Quiere saber c&oacute;mo soy y qui&eacute;n soy?, perm&iacute;tame presentarme a trav&eacute;s de mi VideoCV en&nbsp;<a href="http://es.youtube.com/watch?v=jXDc31_-1IY" target="_blank">castellano</a>&nbsp;y en&nbsp;<a href="http://es.youtube.com/watch?v=61terWU89ws" target="_blank">ingl&eacute;s</a>.</p>
+    <p>Desde hace ya m&aacute;s de 7 a&ntilde;os trabajo como desarrollador freelance para empresas de dise&ntilde;o como&nbsp;<a href="http://www.grafital.com/" target="_blank">Grafital</a>,&nbsp;<a href="http://www.arixta.com/" target="_blank">Arixta</a>,&nbsp;<a href="http://www.artde3.com/" target="_blank">Artde3</a>, etc... aunque tambi&eacute;n realizo desarrollos particulares para clientes como&nbsp;<a href="http://www.scmnetwork.es/" target="_blank">scmnetwork.es</a>,&nbsp;<a href="http://www.primerframe.com/" target="_blank">primerframe.com</a>, etc... En el portfolio puede ver mis &uacute;ltimos trabajos.</p>
+    <p>He tenido experiencias en grandes empresas, por eso tengo el conocimiento necesario para hacerle llegar cuanto antes, lo que realmente usted necesita, sin comerciales, sin intermediarios, sin esperas, con toda la rapidez que usted siempre ha necesitado y con la calidad y fiabilidad que usted requiere.<br />
+    </p>
+    <p>S&eacute; por experiencia que usted ya tiene bastante trabajo en su empresa, y por ello, mi objetivo es no hacerle perder el tiempo de ninguna manera. Hago uso de los conocimientos y la infraestructura para poderle ofrecer<a href="http://www.ayzweb.com/presupuesto-web-online-sin-compromiso.php">&nbsp;un precio competitivo</a>&nbsp;y sin competencia, todo ello, con el nivel de&nbsp;<a href="http://www.ayzweb.com/garantias-de-nuestros-desarrollos-web.php">calidad</a>&nbsp;que espera de una gran empresa, compru&eacute;belo!.</p>
+  <!-- InstanceEndEditable --><!-- end .sidebar1 --></div>
+  
+  <div class="content"><!-- InstanceBeginEditable name="ParteDerecha" -->
   <div class="content">
     <h1>Instrucciones</h1>
     <p>Tenga en cuenta que la CSS para estos dise&ntilde;os cuenta con numerosos comentarios. Si realiza la mayor parte de su trabajo en la vista Dise&ntilde;o, eche un vistazo al c&oacute;digo para obtener sugerencias sobre c&oacute;mo trabajar con la CSS para dise&ntilde;os fijos. Puede quitar estos comentarios antes de lanzar el sitio. Para obtener m&aacute;s informaci&oacute;n sobre las t&eacute;cnicas usadas en estos dise&ntilde;os CSS, lea este art&iacute;culo en el Centro de desarrolladores de Adobe: <a href="http://www.adobe.com/go/adc_css_layouts">http://www.adobe.com/go/adc_css_layouts</a>.</p>
@@ -38,7 +45,10 @@
     <p>Para quitar los estilos en l&iacute;nea, aseg&uacute;rese de que el panel Estilos CSS est&aacute; configurado como Actual. Seleccione la imagen y, en la secci&oacute;n Propiedades del panel Estilos CSS, haga clic con el bot&oacute;n derecho del rat&oacute;n y elimine las propiedades de visualizaci&oacute;n y de fondo. (Por supuesto que siempre podr&aacute; ir directamente al c&oacute;digo y eliminar all&iacute; los estilos en l&iacute;nea de la imagen o el marcador de posici&oacute;n.)</p>
     <h4>Fondos</h4>
     <p>Por naturaleza, el color de fondo de cualquier div s&oacute;lo se muestra a lo largo del contenido. Esto significa que si est&aacute; usando un color de fondo o un borde para crear el aspecto de una columna lateral, &eacute;ste no se extender&aacute; hasta el pie de p&aacute;gina, sino que se detendr&aacute; donde termine el contenido. Si la div de .content siempre va a incluir m&aacute;s contenido, puede colocar un borde en la div de .content para separarla de la columna.</p>
-    <!-- end .content --></div>
+    <!-- end .content -->
+  </div>
+  <!-- InstanceEndEditable -->
+  </div>
   <div class="footer">
   	<?php include("includes/pie.php"); ?>
   </div>
